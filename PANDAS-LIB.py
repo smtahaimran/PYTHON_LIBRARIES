a1f313969e20c592bf85342b12b56c2b6f19182c
Muhammad Taha Imran <smtahaimran@gmail.com>
2025-08-05 12:17:44 -0700

    Update PANDAS-LIB.py

--- PANDAS-LIB.py
+++ PANDAS-LIB.py
@@ -2,28 +2,28 @@ import pandas as pd
 
 #........................................
 
-# a=["Ali","Ahmed","Saad","Haris","Zain"]
-# pt=pd.Series(a)
-# print(pt)
+ a=["Ali","Ahmed","Saad","Haris","Zain"]
+ pt=pd.Series(a)
+ print(pt)
 
 #........................................
 #.........................................
 
-# name=["Ali","Ahmed","Saad","Haris","Zain"]
-# pt=pd.Series(name,index=["a","b","c","d","e"])
-# print(pt)
+ name=["Ali","Ahmed","Saad","Haris","Zain"]
+ pt=pd.Series(name,index=["a","b","c","d","e"])
+ print(pt)
 
 #.......................................
 # Data Frame
 #.......................................
 
-# data={
-#     "Name":["Ali","Ahmed","Zain","Haris","Saad"],
-#     "age":[20,22,23,19,27],
-#     "city":["Lahore","Islamabad","Karachi","Lahore","Sialkot"],
-# }
+ data={
+     "Name":["Ali","Ahmed","Zain","Haris","Saad"],
+     "age":[20,22,23,19,27],
+     "city":["Lahore","Islamabad","Karachi","Lahore","Sialkot"],
+ }
 
-# print(pd.DataFrame(data))
+ print(pd.DataFrame(data))
 
 #.......................................
 # From CSV
@@ -37,4 +37,5 @@ print(df.head(5))
 
 # Below 5
 print(df.tail(5))
+
 #.........................................
